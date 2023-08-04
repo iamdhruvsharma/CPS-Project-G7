02625628b4b10b151841ca4df6ec47f970b1dcf9
iamdhruvsharma <108026681+iamdhruvsharma@users.noreply.github.com>
2023-08-04 19:31:42 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,12 @@
 import telebot
 
-bot_token = '5809770346:AAGA2sLrBDyzMY4Fq2rR3o6YkHv2U9l-OCk'
+bot_token = ''
 
 bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    chat_id = 5980789960
+    chat_id = 
     bot.send_message(chat_id, "Hello")
 
 bot.polling()
